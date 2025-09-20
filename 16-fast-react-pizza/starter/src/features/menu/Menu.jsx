bd7617743b3205788acea9e786bbe5a03d27cc2d
Xiangyu Dong <slam.printer.08@icloud.com>
2025-09-19 17:35:46 -0700

    My React learning note Section 22: React Router With Data Loading (v6.4+)

---
+++ 16-fast-react-pizza/starter/src/features/menu/Menu.jsx
@@ -0,0 +1,22 @@
+import { getMenu } from "../../services/apiRestaurant.js";
+import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem.jsx";
+
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
---
+++ 16-fast-react-pizza/starter/src/ui/AppLayout.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header.jsx";
+import Loader from "./Loader.jsx";
+import CartOverview from "../features/cart/CartOverview.jsx";
+import { Outlet, useNavigation } from "react-router-dom";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
