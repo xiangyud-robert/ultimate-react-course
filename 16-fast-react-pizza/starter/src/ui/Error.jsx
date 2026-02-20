7d53671473651b1e72c7181c54c6e9906e9fb8ab
Xiangyu Dong <slam.printer.08@icloud.com>
2026-02-19 23:47:25 -0800

    My React learning note Section 23: Tailwind CSS Crash Course: Styling the App

--- 16-fast-react-pizza/starter/src/ui/Error.jsx
+++ 16-fast-react-pizza/starter/src/ui/Error.jsx
@@ -1,7 +1,7 @@
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
+import LinkButton from './LinkButton.jsx';
 
 function Error() {
-  const navigate = useNavigate();
   const error = useRouteError();
   console.log(error);
 
@@ -9,7 +9,8 @@ function Error() {
     <div>
       <h1>Something went wrong 😢</h1>
       <p>{error.data || error.message}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+
+      <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
 }
