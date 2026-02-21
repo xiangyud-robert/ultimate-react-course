654759e60f3e948b5845b522c063299b8eb2f70c
Xiangyu Dong <slam.printer.08@icloud.com>
2026-02-21 12:04:58 -0800

    My React learning note Section 24: Adding Redux and Advanced React Router

--- 16-fast-react-pizza/starter/src/App.jsx
+++ 16-fast-react-pizza/starter/src/App.jsx
@@ -1,14 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from "./ui/Home.jsx";
-import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
-import Cart from "./features/cart/Cart.jsx";
+import Home from './ui/Home.jsx';
+import Error from './ui/Error.jsx';
+import Menu, { loader as menuLoader } from './features/menu/Menu.jsx';
+import Cart from './features/cart/Cart.jsx';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder.jsx";
-import Order, { loader as orderLoader } from "./features/order/Order.jsx";
-import AppLayout from "./ui/AppLayout.jsx";
-import Error from "./ui/Error.jsx";
+} from './features/order/CreateOrder.jsx';
+import Order, { loader as orderLoader } from './features/order/Order.jsx';
+import { action as updateOrderAction } from './features/order/UpdateOrder.jsx';
+
+import AppLayout from './ui/AppLayout.jsx';
 
 const router = createBrowserRouter([
   {
@@ -17,26 +19,27 @@ const router = createBrowserRouter([
 
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
