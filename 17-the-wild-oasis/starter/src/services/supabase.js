1beeda5438f6ffba12727736835cf3849ea0e639
Xiangyu Dong <slam.printer.08@icloud.com>
2026-02-23 20:05:11 -0800

    My React learning note Section 26: Supabase Crash Course: Building a Back-End!

---
+++ 17-the-wild-oasis/starter/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ikugrhbivvpdbvednqoo.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrdWdyaGJpdnZwZGJ2ZWRucW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4ODg2OTQsImV4cCI6MjA4NzQ2NDY5NH0.7gx6B2hPA3hl5halJWqHfL8tNrbCQxIWBgnhQfDwyec";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
