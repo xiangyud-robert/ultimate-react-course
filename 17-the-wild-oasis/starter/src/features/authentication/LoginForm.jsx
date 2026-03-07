838156ee0af2b3ff0ee83ec52e04b5eee2618806
Xiangyu Dong <slam.printer.08@icloud.com>
2026-03-06 22:09:59 -0800

    My React learning note Section 30: Deployment With Netlify and Vercel

--- 17-the-wild-oasis/starter/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/starter/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("jonas@example.com");
-  const [password, setPassword] = useState("pass0987");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
